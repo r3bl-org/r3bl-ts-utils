e7b3e4abb975fc5173da07b8ee0224caca64ef8c
Nazmul Idris <nazmul@fasterlap.com>
2022-01-06 20:36:02 -0800

    Change ConfirmInput to leverage useInput
    Instead of using useNodeKeypress, leverage useInput so that there isn't
    more than one (node readline) keypress event listener that's registered.
    
    Ultimately focus management has to be used in order to get input events
    routed correctly to the intended component (that is visible). And there
    can be many visible components that can accept focus.
    
    More info: https://github.com/r3bl-org/r3bl-ts-utils/issues/2
    
    Fixes #2

--- src/ink-ui/confirm-input.tsx
+++ src/ink-ui/confirm-input.tsx
@@ -15,12 +15,15 @@
  *
  */
 
+import { useInput } from "ink"
 import TextInput from "ink-text-input"
+import { Key } from "ink/build/hooks/use-input"
 import _ from "lodash"
 import React, { FC } from "react"
-import { _callIfTrue, StateHook, useKeyboard, useStateSafely } from "../index"
+import { _callIfTrue, Keypress, StateHook, useKeyboard, useStateSafely } from "../index"
 
 const DEBUG = false
+const FLAG_HANDLE_INPUT_IMPL: "useInput" | "useNodeKeypress" = "useInput"
 
 interface Props {
   placeholderBeforeSubmit: string
@@ -34,14 +37,23 @@ export const ConfirmInput: FC<Props> =
     const [ value, setValue ]: StateHook<string> = useStateSafely("").asArray()
     const [ showCursor, setShowCursor ]: StateHook<boolean> = useStateSafely(true).asArray()
     
-    useKeyboard(input => _callIfTrue(
-      _.includes([ "backspace", "delete" ], input.toString()),
-      () => {
-        if (!showCursor) return
-        setValue("")
+    if (FLAG_HANDLE_INPUT_IMPL === "useInput") {
+      const fun = (_input: string, key: Key) => {
+        if (key.backspace || key.delete) setValue("")
         DEBUG && console.log("ConfirmInput ... keypress detected")
       }
-    ), [ showCursor ])
+      useInput(fun, { isActive: showCursor })
+    } else {
+      const fun = (input: Readonly<Keypress>) => _callIfTrue(
+        _.includes([ "backspace", "delete" ], input.toString()),
+        () => {
+          if (!showCursor) return
+          setValue("")
+          DEBUG && console.log("ConfirmInput ... keypress detected")
+        }
+      )
+      useKeyboard(fun, [ showCursor ])
+    }
     
     const onSubmitHandler = (userInput: string) => {
       onSubmit(userInput ? userInput === "y" : defaultValue)
