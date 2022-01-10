8cb4aafddbd05d556f7304d51fbb2b29effd85c9
Nazmul Idris <nazmul@fasterlap.com>
2022-01-10 15:15:28 -0800

    1.0.87

--- src/__tests__/kotlin-lang-utils.test.ts
+++ src/__tests__/kotlin-lang-utils.test.ts
@@ -16,73 +16,75 @@
  */
 
 import {
-  _also, _alsoAsync, _alsoSafe, _alsoSafeAsync, _apply, _let, _letSafe, _then, _with,
-  ImplicitReceiverObject, ImplicitReceiverObjectWithReturn, ReceiverFn, ReceiverFnAsync,
+  _also,
+  _alsoAsync,
+  _alsoSafe,
+  _alsoSafeAsync,
+  _apply,
+  _let,
+  _letSafe,
+  _then,
+  _with,
+  ImplicitReceiverObject,
+  ImplicitReceiverObjectWithReturn,
+  ReceiverFn,
+  ReceiverFnAsync,
 } from "../index"
 
-it(
-  "_then() takes a contextObject, passes it to the ReceiverFn[], and returns the contextObject",
-  () => {
-    const contextObject = "_then"
-    
-    const flags = [ false, false ]
-    const fun1: ReceiverFn<string> = (it) => {
-      expect(it).toEqual(contextObject)
-      flags[0] = true
-    }
-    const fun2: ReceiverFn<string> = (it) => {
-      expect(it).toEqual(contextObject)
-      flags[1] = true
-    }
-    
-    const returnValue = _then(contextObject, fun1, fun2)
-    expect(returnValue).toEqual(contextObject)
-    expect(flags[0]).toBeTruthy()
-    expect(flags[1]).toBeTruthy()
-  }
-)
+it("_then() takes a contextObject, passes it to the ReceiverFn[], and returns the contextObject", () => {
+  const contextObject = "_then"
 
-it(
-  "_also() takes a contextObject, passes it to the ReceiverFn, and returns the contextObject",
-  () => {
-    const contextObject = "_also"
-    let flag = false
-    const returnValue = _also(contextObject, it => {
-      expect(it).toBe(contextObject)
-      flag = true
-    })
-    expect(returnValue).toEqual(contextObject)
-    expect(flag).toBeTruthy()
+  const flags = [false, false]
+  const fun1: ReceiverFn<string> = (it) => {
+    expect(it).toEqual(contextObject)
+    flags[0] = true
   }
-)
+  const fun2: ReceiverFn<string> = (it) => {
+    expect(it).toEqual(contextObject)
+    flags[1] = true
+  }
 
-it(
-  "_alsoSafe() takes a contextObject, passes a deep copy of it to the ReceiverFn, and returns the copy",
-  () => {
-    const contextObject = { foo: 1 }
-    
-    let flag = false
-    const fun: ReceiverFn<typeof contextObject> = (it: typeof contextObject): void => {
-      expect(it).not.toBe(contextObject)
-      flag = true
-    }
-    
-    const returnValue = _alsoSafe(contextObject, fun)
-    
-    expect(returnValue).not.toBe(contextObject)
-    expect(returnValue).toEqual(contextObject)
-    expect(flag).toBeTruthy()
+  const returnValue = _then(contextObject, fun1, fun2)
+  expect(returnValue).toEqual(contextObject)
+  expect(flags[0]).toBeTruthy()
+  expect(flags[1]).toBeTruthy()
+})
+
+it("_also() takes a contextObject, passes it to the ReceiverFn, and returns the contextObject", () => {
+  const contextObject = "_also"
+  let flag = false
+  const returnValue = _also(contextObject, (it) => {
+    expect(it).toBe(contextObject)
+    flag = true
+  })
+  expect(returnValue).toEqual(contextObject)
+  expect(flag).toBeTruthy()
+})
+
+it("_alsoSafe() takes a contextObject, passes a deep copy of it to the ReceiverFn, and returns the copy", () => {
+  const contextObject = { foo: 1 }
+
+  let flag = false
+  const fun: ReceiverFn<typeof contextObject> = (it: typeof contextObject): void => {
+    expect(it).not.toBe(contextObject)
+    flag = true
   }
-)
+
+  const returnValue = _alsoSafe(contextObject, fun)
+
+  expect(returnValue).not.toBe(contextObject)
+  expect(returnValue).toEqual(contextObject)
+  expect(flag).toBeTruthy()
+})
 
 it(
   "_alsoAsync() takes a contextObject, passes it to the ReceiverFnAsync, and returns it and" +
-  " promise from ReceiverFnAsync",
+    " promise from ReceiverFnAsync",
   async () => {
     const contextObject = "_alsoAsync"
-    
+
     let flag = false
-    
+
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
     const fun1: ReceiverFnAsync<string, boolean> = async (it) =>
       new Promise<boolean>((resolveFn) => {
@@ -95,12 +97,12 @@ it(
         // https://nodejs.dev/learn/understanding-setimmediate
         setImmediate(_fun)
       })
-    
+
     const { contextObject: obj, promiseFromReceiverFn: promise } = _alsoAsync(contextObject, fun1)
-    
+
     expect(obj).toEqual(contextObject)
     expect(flag).toBeFalsy()
-    
+
     const value = await promise
     expect(value).toBeTruthy()
     expect(flag).toBeTruthy()
@@ -109,12 +111,12 @@ it(
 
 it(
   "_alsoSafeAsync() takes a contextObject, passes a deep copy of it to the ReceiverFnAsync," +
-  " and returns the deep copy and the promise from ReceiverFnAsync",
+    " and returns the deep copy and the promise from ReceiverFnAsync",
   async () => {
     const contextObject = { foo: 1 }
-    
+
     let flag = false
-    
+
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
     const fun1: ReceiverFnAsync<typeof contextObject, boolean> = async (it) =>
       new Promise<boolean>((resolveFn) => {
@@ -128,43 +130,40 @@ it(
         // https://nodejs.dev/learn/understanding-setimmediate
         setImmediate(_fun)
       })
-    
+
     const { contextObjectDeepCopy: obj, promiseFromReceiverFn: promise } = _alsoSafeAsync(
       contextObject,
       fun1
     )
-    
+
     expect(obj).toEqual(contextObject)
     expect(flag).toBeFalsy()
-    
+
     const value = await promise
     expect(value).toBeTruthy()
     expect(flag).toBeTruthy()
   }
 )
 
-it(
-  "_let() takes a contextObject, passes it to the ReceiverFnWithReturn, and returns its return value",
-  () => {
-    const contextObject = "_let"
-    let flag = false
-    const returnValue = _let(contextObject, it => {
-      expect(contextObject).toBe(it)
-      flag = true
-      return "foo"
-    })
-    expect(returnValue).toEqual("foo")
-    expect(flag).toBeTruthy()
-  }
-)
+it("_let() takes a contextObject, passes it to the ReceiverFnWithReturn, and returns its return value", () => {
+  const contextObject = "_let"
+  let flag = false
+  const returnValue = _let(contextObject, (it) => {
+    expect(contextObject).toBe(it)
+    flag = true
+    return "foo"
+  })
+  expect(returnValue).toEqual("foo")
+  expect(flag).toBeTruthy()
+})
 
 it(
   "_letSafe() takes a contextObject, passes a deep copy of it to the ReceiverFnWithReturn, and" +
-  " returns its return value",
+    " returns its return value",
   () => {
     const contextObject = { foo: 1 }
     let flag = false
-    const returnValue = _letSafe(contextObject, it => {
+    const returnValue = _letSafe(contextObject, (it) => {
       expect(it).toEqual(contextObject)
       expect(it).not.toBe(contextObject)
       flag = true
@@ -177,7 +176,7 @@ it(
 
 it(
   "_apply() takes a contextObject, binds it to ImplicitReceiverObject's this, calls it, then" +
-  " returns the contextObject",
+    " returns the contextObject",
   () => {
     const contextObject = "_apply"
     let flag = false
@@ -186,7 +185,7 @@ it(
         expect(this).toEqual(contextObject)
         flag = true
         return contextObject
-      }
+      },
     }
     const returnValue = _apply(contextObject, myImplicitReceiverObject)
     expect(returnValue).toEqual(contextObject)
@@ -196,7 +195,7 @@ it(
 
 it(
   "_with() takes a contextObject, binds it to ImplicitReceiverObjectWithReturn's this, calls it," +
-  " then returns the its return value",
+    " then returns the its return value",
   () => {
     const contextObject = "_with"
     let flag = false
