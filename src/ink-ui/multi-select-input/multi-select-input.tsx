507dd9a71ffa0ca018c5e97d70fd18b91dc04f42
Nazmul Idris <nazmul@fasterlap.com>
2022-01-13 14:43:28 -0800

    Update comments

--- src/ink-ui/multi-select-input/multi-select-input.tsx
+++ src/ink-ui/multi-select-input/multi-select-input.tsx
@@ -74,6 +74,7 @@ export const MultiSelectInput: FC<MultiSelectInputProps> = ({
     ({ emitter, eventName }) => {
       // https://stackoverflow.com/questions/53898810/executing-async-code-on-update-of-state-with-react-hooks
       // https://github.com/r3bl-org/r3bl-ts-utils/commit/a3248540ea325d3896ee56a84d003f15529169cd
+      // https://github.com/r3bl-org/r3bl-ts-utils/commit/1f3cbb2b4988f44c6ea48233db1730e10f18dc60
       // http://developerlife.com/2021/10/19/react-hooks-redux-typescript-handbook/#custom-hooks
       useEffect(
         () => {emitter.on(eventName, onKeypress)},
